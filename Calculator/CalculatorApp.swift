07eac1cc0740cdad4603e2e9513bbfc497fc4fbe
Michael Vlamis <michael.vlamis@icloud.com>
2023-06-07 19:18:08 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Michael Vlamis on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
