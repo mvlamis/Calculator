ceea9e64aad1d840f28f9527e237d772319b16d6
Michael Vlamis <michael.vlamis@icloud.com>
2023-06-19 12:32:56 -0400

    settings page

---
+++ Calculator/VariablesView.swift
@@ -0,0 +1,18 @@
+//
+//  VariablesView.swift
+//  Calculator
+//
+//  Created by Michael Vlamis on 2023-06-17.
+//
+
+import SwiftUI
+
+struct VariablesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VariablesView()
+}
